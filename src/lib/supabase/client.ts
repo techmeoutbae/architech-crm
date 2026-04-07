8edece5b2506538942098dd0dc15673ce1b9f4e5
Lillian Hannah <techmeoutbae@gmail.com>
2026-04-07 15:26:36 -0400

    Add fallback env values for Vercel

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://glfztcujpgyivhftxexr.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsZnp0Y3VqcGd5aXZoZnR4ZXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODAxNDAsImV4cCI6MjA5MTE1NjE0MH0.DA4j8z6dU21aJ-ofcvo-IXTilhbrZHPJhk7a7sUB1bo'
   )
 }
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -5,8 +5,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://glfztcujpgyivhftxexr.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsZnp0Y3VqcGd5aXZoZnR4ZXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODAxNDAsImV4cCI6MjA5MTE1NjE0MH0.DA4j8z6dU21aJ-ofcvo-IXTilhbrZHPJhk7a7sUB1bo',
     {
       cookies: {
         getAll() {
